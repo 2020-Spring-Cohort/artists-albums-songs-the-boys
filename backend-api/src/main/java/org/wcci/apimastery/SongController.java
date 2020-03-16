621ee69d3f7d1ccaa5bccd5409e0aa2b7090407a
ja-br <letstalk@jamesbrooks.tech>
2020-03-16 14:56:16 -0400

    Songs endpoint http request returns OK

--- backend-api/src/main/java/org/wcci/apimastery/SongController.java
+++ backend-api/src/main/java/org/wcci/apimastery/SongController.java
@@ -1,5 +1,6 @@
 package org.wcci.apimastery;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
@@ -9,12 +10,11 @@ public class SongController {
 
     private SongRepository songRepo;
 
-    SongController(){}
-
     SongController(SongRepository songRepo){
         this.songRepo = songRepo;
     }
 
+    @RequestMapping("/songs")
     public Collection<Song> retrieveSongs() {
         return (Collection<Song>) songRepo.findAll();
     }
