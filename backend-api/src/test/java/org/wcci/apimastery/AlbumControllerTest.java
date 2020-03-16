81b61a7b5903baa955e73e8f55d8df94334f1ad9
abdul jama <jamaabdul23@gmail.com>
2020-03-16 14:08:37 -0400

    made autowiring test

--- backend-api/src/test/java/org/wcci/apimastery/AlbumControllerTest.java
+++ backend-api/src/test/java/org/wcci/apimastery/AlbumControllerTest.java
@@ -1,13 +1,19 @@
 package org.wcci.apimastery;
 
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 import java.util.Collection;
 import java.util.Collections;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class AlbumControllerTest {
 
@@ -38,6 +44,17 @@ public class AlbumControllerTest {
     public void retrieveAlbumsReturnsListOfAlbumsContainingMockAlbums() {
         Collection<Album> result= underTest.retrieveAlbums();
         assertThat(result).contains(testAlbum);
+
+    }
+
+    @Test
+
+    public void controllerIsWiredCorrectly() throws Exception
+      {
+          MockMvc mockMvc = MockMvcBuilders.standaloneSetup(underTest).build();
+          mockMvc.perform(get("/albums"))
+          .andExpect(status().isOk());
+
 
     }
 
