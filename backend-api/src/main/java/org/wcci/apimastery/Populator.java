2fa9bf6084d5057b9ed7c787704440c15fc35e9e
Noah Doughty <noahdoughty@gmail.com>
2020-03-16 14:48:51 -0400

    Added albums into Populator and switched constructor order in Album class

--- backend-api/src/main/java/org/wcci/apimastery/Populator.java
+++ backend-api/src/main/java/org/wcci/apimastery/Populator.java
@@ -30,5 +30,30 @@ public class Populator implements CommandLineRunner {
         artistRepository.save(sufjanStevens);
         Artist weirdAlYankovic = new Artist("Weird Al Yankovic");
         artistRepository.save(weirdAlYankovic);
+        Album theEndOfSilence = new Album("The End Of Silence", rollinsBand);
+        albumRepository.save(theEndOfSilence);
+        Album aNicerShadeOfRed = new Album("A Nicer Shade Of Red", rollinsBand);
+        albumRepository.save(aNicerShadeOfRed);
+        Album ricePasta = new Album("Rice, Pasta, and Other Fillers", porridgeRadio);
+        albumRepository.save(ricePasta);
+        Album everyBad = new Album("Every Bad", porridgeRadio);
+        albumRepository.save(everyBad);
+        Album nothingSame = new Album("Nothing Was The Same", drake);
+        albumRepository.save(nothingSame);
+        Album views = new Album("Views", drake);
+        albumRepository.save(views);
+        Album closerToSun = new Album("Closer To The Sun", slightlyStoopid);
+        albumRepository.save(closerToSun);
+        Album everythingNeed = new Album("Everything You Need", slightlyStoopid);
+        albumRepository.save(everythingNeed);
+        Album ageAdz = new Album("The Age Of Adz", sufjanStevens);
+        albumRepository.save(ageAdz);
+        Album carrieLowell = new Album("Carrie & Lowell", sufjanStevens);
+        albumRepository.save(carrieLowell);
+        Album badHair = new Album("Bad Hair Day", weirdAlYankovic);
+        albumRepository.save(badHair);
+        Album runningScissors = new Album("Running With Scissors", weirdAlYankovic);
+        albumRepository.save(runningScissors);
+
     }
 }
