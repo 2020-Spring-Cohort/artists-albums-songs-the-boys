51aefffc2d614134bd5de0d6a19756aebb2083b3
ja-br <letstalk@jamesbrooks.tech>
2020-03-17 13:55:06 -0400

    updated with SongControllerImpTest and SongStorageJpaImpl classes

---
+++ backend-api/src/main/java/org/wcci/apimastery/SongNotFoundException.java
@@ -0,0 +1,12 @@
+package org.wcci.apimastery;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class SongNotFoundException extends Throwable {
+    public SongNotFoundException(String message) {
+
+    }
+}
---
+++ backend-api/src/main/java/org/wcci/apimastery/SongStorageJpaImpl.java
@@ -0,0 +1,35 @@
+package org.wcci.apimastery;
+
+
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+
+@Service
+public class SongStorageJpaImpl implements SongStorage{
+    private final SongRepository songRepo;
+
+    public SongStorageJpaImpl(SongRepository songRepo){
+        this.songRepo = songRepo;
+    }
+@Override
+    public Collection<Song> findAllSongs(){
+        return (Collection<Song>) songRepo.findAll();
+}
+@Override
+public void store(Song song){
+        songRepo.save(song);
+}
+@Override
+    public Song findSongByTitle(String songTitle) throws SongNotFoundException {
+        Song retrievedSong;
+            try{
+                retrievedSong = songRepo.findByTitle(songTitle).get();
+            }
+            catch (Exception e){
+                throw new SongNotFoundException(e.getMessage());
+            }
+            return retrievedSong;
+}
+
+}
