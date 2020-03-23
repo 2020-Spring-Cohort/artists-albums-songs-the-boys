e92c0bbfdc82b1e9c734bb7f25f9046f520e76c0
ja-br <letstalk@jamesbrooks.tech>
2020-03-23 14:42:33 -0400

    Created script.js, have artist list displaying on page through fetch

---
+++ frontend-spa/src/js/script.js
@@ -0,0 +1,77 @@
+const artistListAnchorElement = document.querySelector('.artistListAnchor');
+
+class DomMaker {
+
+    constructor(elementType) {
+        this.htmlElement = document.createElement(elementType);
+    }
+
+    appendChild(element) {
+        this.htmlElement.append(element);
+        return this;
+    }
+
+    changeContent(newContent) {
+        this.htmlElement.innerText = newContent;
+        return this;
+    }
+
+    render() {
+        return this.htmlElement;
+    }
+
+    addClassName(className){
+        this.htmlElement.classList.add(className)
+        return this;
+    }
+
+    addOnClickAction(functionToRun){
+        this.htmlElement.addEventListener('click', (event)=>{
+            event.preventDefault();
+            functionToRun();
+        })
+        return this;
+    }
+}
+
+class InputMaker extends DomMaker{
+
+    constructor(){
+        super('input');
+    }
+    changeInputType(type){
+        this.htmlElement.setAttribute('type',type);
+        return this;
+    }
+    addPlaceholder(placeholder){
+        this.htmlElement.setAttribute('placeholder',placeholder);
+        return this;
+    }
+}
+
+const fetchAllArtists = () => {
+    fetch('http://localhost:8080/artists/')
+        .then(response => response.json())
+        .then(artists => displayArtists(artists));
+}
+
+const displayArtists = (artists) => {
+    artistListAnchorElement.innerHTML='';
+    let artistList = new DomMaker('ul')
+        .changeContent('List of artists');
+
+    artists.forEach((artist) => {
+        artistList.appendChild(new DomMaker('a')
+            .changeContent(artist.name)
+            .render())
+    });
+
+    artistListAnchorElement.append(artistList.render());
+}
+
+
+    fetchAllArtists();
+
+
+
+
